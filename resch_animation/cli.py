65a70e58bb52c7660c62f5448714796b28a0b6f2
ymiyazawa <y.r.miyazawa@gmail.com>
2022-11-08 13:26:11 -0800

    Add resch animation package

---
+++ resch_animation/cli.py
@@ -0,0 +1,57 @@
+def main():
+    import numpy as np
+    import matplotlib.pyplot as plt
+    plt.style.use('./common/custom.mplstyle')  # Relative to root directory
+
+    import argparse
+
+    plt.style.use('./common/custom.mplstyle')  # Relative to root directory
+
+    from .lib.resch_hex_static import ReschOrigamiAnalysis
+    roa = ReschOrigamiAnalysis()
+
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('-la', '--lengtha', help='Side length of crease "a" (float, default=1.0)', type=float, default=1.0)
+    parser.add_argument('-nbin', '--nbin', help='Number of steps', type=int, default=64)
+    parser.add_argument('-th0', '--theta0', help='Initial fold angle (float, default=0 deg)', type=float, default=0.0)
+    parser.add_argument('-thf', '--thetaf', help='Final fold angle (float, default=90 deg)', type=float, default=90.0)
+    parser.add_argument('-zip', '--savezip', help='Option to compress vtk files into zip file (True|False, default=False)', type=bool, default=False)
+    parser.add_argument('-fig', '--figout', help='Option to display plots (True|False, default=False)', type=bool, default=False)
+    parser.add_argument('-lkup', '--lookuptab',
+                        help='Option to use lookup table instead of solving fold angle (True|False, default=True)',
+                        type=bool, default=True)
+
+    args = parser.parse_args()
+    La = args.lengtha
+    nbin = args.nbin
+    theta0 = np.radians(args.theta0)
+    thetaf = np.radians(args.thetaf)
+    fig_out = args.figout
+    save_zip = args.savezip
+    use_lookuptab = args.lookuptab
+
+    theta_M = np.linspace(theta0, thetaf, nbin)
+
+    if use_lookuptab:
+        theta_MKJS = None
+    else:
+        xx = np.linspace(0.0, 0.5 * np.pi, int(2 * nbin))
+        theta_MKJS = roa.find_theta_MKJS(theta_M=xx,
+                                         La=La, x0=np.radians(0.1),
+                                         save_lookuptab=True,
+                                         )
+
+    roa.geo_init(La=La,
+                 theta_M0=theta_M[0],
+                 theta_MKJS=theta_MKJS,
+                 use_lookuptab=use_lookuptab,
+                 )
+
+    roa.solve_geo_1orbit(theta_M=theta_M,
+                         vtk_out=True,
+                         fig_out=fig_out,
+                         save_zip=save_zip)
+
+    plt.show()
+
+    return
